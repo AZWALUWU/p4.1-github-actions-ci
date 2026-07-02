07c5eb3dbab5695609b79ac01693ce32d2c3fb26
AZWALUWU <azwaluwu472@gmail.com>
2026-07-02 10:20:43 +0700

    feat: menambahkan workflow CI dengan lint dan matrix test

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,15 @@
-// eslint.config.js
-export default [
+module.exports = [
     {
+        files: ["**/*.js"],
+        languageOptions: {
+            sourceType: "commonjs",
+            globals: {
+                test: "readonly",
+                expect: "readonly",
+                module: "readonly",
+                require: "readonly"
+            }
+        },
         rules: {
             "no-unused-vars": "error",
             "no-undef": "error"
